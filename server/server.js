2fa42eb3478ccb1e8da0bb639f118f4a699f4a3d
Alan Ma <alan_ma@live.ca>
2018-05-07 16:10:24 -0400

    switched to socket io test events

--- server/server.js
+++ server/server.js
@@ -1,11 +1,61 @@
+// declare necessary dependencies
 var express = require('express');
-var PythonShell = require('python-shell');
+var http = require('http');
+
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+// python shell to execute python scipts
+var PythonShell = require('python-shell');
+
+// serve static public files
+app.use(express.static('public'));
+
+// LED Grid
+var LEDGrid = ['empty', 'empty'];
+
+// establish socket io connection
+io.on('connection', function(socket) {
+  // initialize client
+  socket.emit('updateClient', { grid: LEDGrid });
+
+  // receive client input
+  socket.on('updateServer', function(data) {
+    LEDGrid = data.grid;
+    // update the client
+    socket.emit('updateClient', { grid: LEDGrid });
+
+    // update the physical LED grid
+    updateGrid();
+
+    // log the event
+    console.log('\nserver updated');
+    console.log(LEDGrid);
+  });
+});
+
+
+// update the physical LED grid
+function updateGrid(gridInput) {
+  var options = {
+    args: gridInput
+  };
+
+  // run a python shell to execute the script via a child process
+  PythonShell.run('../test.py', options, function (err, data) {
+    return !err;
+  });
+}
+
+/*
+// route handler (endpoint) to execute python script
 
 app.get('/test', test);
 
 function test(req, res) {
-  // http://localhost:3000/test?message1=hello&message2=world
+  // testing endpoint http://localhost:3000/test?message1=hello&message2=world
+  // options for the script
   var options = {
     args:
     [
@@ -14,16 +64,18 @@ function test(req, res) {
     ]
   };
 
+  // use python shell to run script via a child process
   PythonShell.run('../test.py', options, function (err, data) {
     if (err) res.send(err);
-    console.log(data);
+    // console.log(data);
     res.send(data.toString());
   });
 }
+*/
 
-
-app.listen(3000, function () {
+server.listen(3000, function () {
   console.log('server running on port 3000');
 });
+
 
 
