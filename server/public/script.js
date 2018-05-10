873b3b55f89e7a37f93e871d9881195ba6f44fe3
Alan Ma <alan_ma@live.ca>
2018-05-10 14:38:43 -0400

    added led grid on client

--- server/public/script.js
+++ server/public/script.js
@@ -5,7 +5,7 @@ led grid
 
 // LED Grid initialization
 var LEDGrid = [];
-var LED_COUNT = 10;
+var LED_COUNT = 100;
 for (i=0; i<LED_COUNT; i++) {
   LEDGrid.push({
     'id': i,
@@ -53,30 +53,35 @@ var app = new Vue({
         'id': 1,
         'name': 'Player1',
         'points': 0,
+        'score': 0,
         'icon': 'fa-star'
       },
       {
         'id': 2,
         'name': 'Player2',
         'points': 0,
+        'score': 0,
         'icon': 'fa-square'
       },
       {
         'id': 3,
         'name': 'Player3',
         'points': 0,
+        'score': 0,
         'icon': 'fa-circle'
       },
       {
         'id': 4,
         'name': 'Player4',
         'points': 0,
+        'score': 0,
         'icon': 'fa-heart'
       },
       {
         'id': 5,
         'name': 'Player5',
         'points': 0,
+        'score': 0,
         'icon': 'fa-play'
       }
     ],
