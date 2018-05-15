f8eb8d65e97f1944b0f34a2a20d6a001a482d0e5
Alan Ma <alan_ma@live.ca>
2018-05-15 17:27:53 -0400

    added clear leds file

--- clear.py
+++ clear.py
@@ -1,10 +1,13 @@
+
 """
-updates the led grid according to the input specifications
+clears the led grid
 uses a python wrapper (rpi_ws281x) for the neopixel library
 """
 
 import sys
 from neopixel import *
+
+print("STARTED")
 
 # LED strip configuration:
 LED_COUNT   = 10      # Number of LED pixels.
@@ -13,18 +16,10 @@ LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA     = 5       # DMA channel to use for generating signal (try 5)
 LED_INVERT  = False   # True to invert the signal (when using NPN transistor level shift)
 
-# Parse the input
-SYS_INPUT = str(sys.argv[1]).split('\n') # each LED is newline separated
-SET_LED_COLOURS = [] # initialize the list
-
-for i in range(len(SYS_INPUT)):
-  new_item = SYS_INPUT.split(',') # each LED specification is comma separated
-  SET_LED_COLOURS.append([]) # add an empty list
-  for j in range(len(new_item)):
-    try:
-      SET_LED_COLOURS[i].append(int(new_item[j])) # try adding the number
-    except ValueError:
-      SET_LED_COLOURS[i].append(0) # add 0 if NaN
+# LED Grid initialization
+LEDGrid = []
+for i in range(LED_COUNT):
+  LEDGrid.append([0, 0, 0]);
 
 
 # Define functions which animate LEDs in various ways.
@@ -34,7 +29,7 @@ def setColours(strip, set_colours):
     strip.setPixelColor(i, Color(
       set_colours[i][0], set_colours[i][1], set_colours[i][2])
     )
-  
+
   strip.show()
 
 
@@ -44,4 +39,7 @@ strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
-setColours(strip, SET_LED_COLOURS)
+setColours(strip, LEDGrid)
+
+print("FINISHED")
+sys.stdout.flush()
